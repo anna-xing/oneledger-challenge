6b0adcea843b2f20c2bb44bb65d8aa96462686c0
anna-xing <anna.xing.22@gmail.com>
2020-09-26 11:01:02 -0400

    set up blockchain imports into App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Card from './components'
+import Card from "./components";
 
-// Predefined Oneledger transaction stuff -----------------------
-
-const yourMasterKeyPassword = "5h$mY_SupEr_sTRong_%$passWorD123#@"; 
-// ^ password to encrypt/decrypt your HD wallet master key
-const fullnodeUrl = "https://comm-test-fn1.devnet.oneledger.network/jsonrpc" 
-// fullnode URL is used to broadcast transactions and make queries
-const faucetServerUrl = "https://comm-test-faucet.devnet.oneledger.network/jsonrpc"; 
-// faucet server URL is used to request Test OLT
-
-// 1. Create HD Wallet
-
-const HDVault = require('hd-vault');
-const mnemonicWords = HDVault.mnemonicUtil.mnemonicGenerator24();
-const masterKey = new HDVault.MasterKeySeedManager(mnemonicWords, yourMasterKeyPassword);
-const {encryptedMasterKeySeed} = masterKey.getMasterKeySeedInfo();
-
-// 2. Create Oneledger Account
-
-const derivedKeyData = {
-	keyType: "OLT",
-	keyIndex: 0, // Increment keyIndex for generating new keys
-	password: yourMasterKeyPassword,
-	encryptedMasterKeySeed: encryptedMasterKeySeed 
-}; 
-
-async function getResponse() {
-	return await HDVault.derivedKeyManager.deriveNewKeyPair(derivedKeyData).catch(error => {
-		throw(error);
-	});
-}
-
-const response = getResponse();
-const {keyIndex, address, publicKey} = response;
-
-// 3. Request Test OLT
-
-const requestAmount = 10000;
-const faucet = require('ons-faucet');
-const env = {url: faucetServerUrl};
-async function getTestResponse() {
-	return await faucet.requestOLT(address, requestAmount, env).catch(error => {
-		throw(error);
-	});
-}
-
-const result = getTestResponse();
-
-// ----------------------------
+const {
+  yourMasterKeyPassword,
+  keyType,
+  keyIndex,
+  publicKey,
+  encryptedMasterKeySeed,
+  env,
+  createWallet,
+  createAccount,
+  requestTestOLT,
+  addPartTx,
+  queryPart,
+  queryBalanceForAddr,
+  sign,
+  broadcastTx,
+} = require("./blockchain/index");
 
 function App() {
-	return (
-		<div className="App">
-		<h1>OneLedger Challenge for BorderHacks!</h1>
-		<Card title="Hello!!" />
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>OneLedger Challenge for BorderHacks!</h1>
+      <Card title="Hello!!" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Card extends React.Component {
     render() {
         return(
-            <div class="card">
-                <h2 class="card-title">{this.props.title}</h2>
+            <div className="card">
+                <h2 className="card-title">{this.props.title}</h2>
             </div>
         );
     }
